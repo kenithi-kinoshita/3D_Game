65d5db8d7144d0a344814380b64163edf6772142
木下健一 <k_k_mac_mini_m1@K-Mac-mini-m1.local>
2021-07-10 16:16:45 +0900

    step16-障害物の追加とそれを除去するエリアの作成、パーティクルの表示と効果音の再生

---
+++ take1/Fairy Garden/Assets/Scripts/EffectScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectScript : MonoBehaviour
+{
+    //　表示するパーティクル
+    [SerializeField]
+    private GameObject particle;
+    private AudioSource audioSource;
+    //　消去中かどうか
+    private bool erasing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //　消去中でなければなにもしない
+        if (!erasing)
+        {
+            return;
+        }
+        //　音声が再生されていなければパーティクルを表示してゲームオブジェクトを非表示にする
+        if (!audioSource.isPlaying)
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+    }
+    //　消失効果処理
+    public void AddEffect()
+    {
+        audioSource.Play();
+        erasing = true;
+    }
+}
---
+++ take1/Fairy Garden/Assets/Scripts/EraseObstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraseObstacle : MonoBehaviour
+{
+    //　無効化するゲームオブジェクト
+    [SerializeField]
+    private GameObject objectOfErase;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //　プレイヤーが侵入したら設定したゲームオブジェクトを無効化
+        if (other.CompareTag("Player"))
+        {
+            //　指定したゲームオブジェクトの効果処理を実行する
+            objectOfErase.GetComponent<EffectScript>().AddEffect();
+            Destroy(gameObject);
+        }
+    }
+}
