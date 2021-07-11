5764c4d5f6c2bc48efb7339e8a2fcdd9ac5fae0e
木下健一 <k_k_mac_mini_m1@K-Mac-mini-m1.local>
2021-07-11 18:57:09 +0900

    step18-ギミック追加-バグあり砲身がプレイヤーの方向を追跡できない・・

---
+++ take1/Fairy Garden/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField]
+    private int attackPower = 1;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        // 弾が発生して10秒後に消す
+        Destroy(gameObject, 10f);
+        return;
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameManager.GameOver)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        //　衝突した相手がプレイヤーならダメージを与え、さらに力を加えて飛ばす
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().TakeDamage(attackPower);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 5f, ForceMode.VelocityChange);
+        }
+        //　何らかのゲームオブジェクトに衝突したら消す
+        Destroy(gameObject);
+    }
+}
